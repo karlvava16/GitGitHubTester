a45482f5295a9b19d84771a6850fec6be832a7dc
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2022-09-06 14:23:28 +0300

    Добавлен метод Print в класс Point. Создан объект класса Point. Выведен результат.

--- GitGitHubTester/main.cpp
+++ GitGitHubTester/main.cpp
@@ -14,10 +14,17 @@ public:
 		x = a;
 		y = b;
 	}
+	void Print()
+	{
+		cout << "X : " << x << endl;
+		cout << "Y : " << y << endl;
+	}
 };
 
 
 int main()
 {
+	Point obg1(5, 1);
+	obg1.Print();
 	return 0;
 }
