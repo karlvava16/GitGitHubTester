c0e46ace38f312ce5783fbb580a10fe8763dcc0c
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2022-09-06 13:57:43 +0300

    Добавлены поля и конструкторы для классов Person, Point.

--- GitGitHubTester/main.cpp
+++ GitGitHubTester/main.cpp
@@ -3,12 +3,34 @@ using namespace std;
 
 class Point
 {
-
+	int x, y;
+public:
+	Point()
+	{
+		x = y = 0;
+	}
+	Point(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
 };
 
 class Person
 {
-
+	string name;
+	int age;
+public:
+	Person()
+	{
+		name = "";
+		age = 0;
+	}
+	Person(string n, int a)
+	{
+		name = n;
+		age = a;
+	}
 };
 
 int main()
