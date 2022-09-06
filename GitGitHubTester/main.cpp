90144499e1e3171dd41dab1dbf11850b0593baad
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2022-09-06 14:13:11 +0300

    Добавлен класс Student без интерфейса.

--- GitGitHubTester/main.cpp
+++ GitGitHubTester/main.cpp
@@ -1,36 +1,12 @@
 #include <iostream>
 using namespace std;
 
-class Point
-{
-	int x, y;
-public:
-	Point()
-	{
-		x = y = 0;
-	}
-	Point(int a, int b)
-	{
-		x = a;
-		y = b;
-	}
-};
-
-class Person
+class Student
 {
 	string name;
 	int age;
 public:
-	Person()
-	{
-		name = "";
-		age = 0;
-	}
-	Person(string n, int a)
-	{
-		name = n;
-		age = a;
-	}
+
 };
 
 int main()
